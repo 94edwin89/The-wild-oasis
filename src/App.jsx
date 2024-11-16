0a694fb724f82d80de9b108e82c53c166ceac955
Edwin Jude <edwin.judefrancis@gmail.com>
2024-11-16 22:24:43 +0530

    introducing to style components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+ import styled from "styled-components";
+ 
+ const H1 = styled.h1`
+  font-weight: 600px;
+  font-family: 'Courier New', Courier, monospace;
+  background-color: yellow;
+ `
+
+ const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 600;
+  border: none;
+  border-radius: 7px;
+  color: white;
+  background-color: purple;
+  cursor: pointer;
+ `
  function App(){
   return(
     <div>
-    Hello World
+    <H1>The Wild Oasis</H1>
+    <Button>hwllo bro</Button>
     </div>
   )
 }
