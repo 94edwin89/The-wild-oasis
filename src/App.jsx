21c00790b7c5fb0710ccdb942a91562a33a5d33b
Edwin Jude <edwin.judefrancis@gmail.com>
2024-11-18 08:59:28 +0530

    styled component props and the css function

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,7 @@ import styled from "styled-components";
 import GlobalState from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
-
-
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -21,9 +14,10 @@ function App() {
     <>
       <GlobalState />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
+        <Heading as="h1">The Wild Oasis</Heading>
         <Button>hwllo bro</Button>
-        <Input type="number" placeholder="number of guest" ></Input>
+        <Heading as="h3">check in</Heading>
+        <Input as="number" placeholder="number of guest"> enter your details</Input>
       </StyledApp>
     </>
   );
