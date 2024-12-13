c1183ae082faff5682f2a061243e119164d449ba
Edwin Jude <edwin.judefrancis@gmail.com>
2024-12-13 21:58:27 +0530

    prefecthing with react query

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   // FITLER
@@ -20,6 +22,7 @@ export function useBookings() {
   // pagination
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
+  // QUERY
   const {
     isLoading,
     data: { data: bookings, count } = {},
@@ -29,5 +32,18 @@ export function useBookings() {
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
+  // PRE-FECTHING
+  const pageCount = Math.ceil(count/PAGE_SIZE)
+
+  if(page<pageCount)
+  queryClient.prefetchQuery({
+    queryKey: ["bookings", filter, sortBy, page + 1],
+    queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+  });
+if(page>1)
+  queryClient.prefetchQuery({
+    queryKey: ["bookings", filter, sortBy, page - 1],
+    queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+  });
   return { isLoading, error, bookings, count };
 }
