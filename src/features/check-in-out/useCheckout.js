c96d545a2d73d0029f57ba92dcdb4e2eee9a47d5
Edwin Jude <edwin.judefrancis@gmail.com>
2024-12-23 20:53:36 +0530

    typo error in code, bug is fixed in checkout, and delete booking

--- src/features/check-in-out/useCheckout.js
+++ src/features/check-in-out/useCheckout.js
@@ -8,7 +8,7 @@ export function useCheckout() {
   
 
   const { mutate: checkout, isLoading: isCheckingOut } = useMutation({
-    mutationFn: ({ bookingId }) =>
+    mutationFn: (bookingId ) =>
       updateBooking(bookingId, {
         status: "checked-out",
       }),
