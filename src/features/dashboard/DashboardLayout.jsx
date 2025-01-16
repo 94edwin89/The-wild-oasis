2a15bd5e31b0772b57922f81812683ef8a22fe6e
94edwin89 <edwin.judefrancis@gmail.com>
2025-01-17 00:43:48 +0530

    displaying stay for current day

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -16,16 +17,11 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { bookings, isLoading: isLoading1 } = useRecentBookings();
-  const {
-    stays,
-    isLoading: isLoading2,
-    confirmStays,
-    numDays,
-  } = useRecentStays();
+  const { isLoading: isLoading2, confirmStays, numDays } = useRecentStays();
   const { cabins, isLoading: isLoading3 } = useCabins();
 
   if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
- 
+
   return (
     <StyledDashboardLayout>
       <Stats
@@ -34,8 +30,8 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Today&apos;s Activities</div>
-      <DurationChart confirmStays={confirmStays}/>
+      <TodayActivity />
+      <DurationChart confirmStays={confirmStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
