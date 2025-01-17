0f281394ecdb02f8f32ca1324ae1ff7a9d2632c1
94edwin89 <edwin.judefrancis@gmail.com>
2025-01-17 14:45:18 +0530

    redeploy

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-
 export const supabaseUrl = "https://cmkondatsegzjeapwsgp.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNta29uZGF0c2VnemplYXB3c2dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwODI3ODYsImV4cCI6MjA0NzY1ODc4Nn0.wM6xouwKWduzLcrDn2_OU-vU_1INep_MlVaG705Lr_Q";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
